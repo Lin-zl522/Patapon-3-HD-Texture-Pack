a6e58a134510ad290bf684bd3fc927d08f856b87
Hozzzd <92305508+Hozzzd@users.noreply.github.com>
2023-06-16 02:53:01 +0400

    textures.ini generator is better now
    it's faster and less stupid !
    i also removed the comments because they were unnecessary

--- textures.ini generator.py
+++ textures.ini generator.py
@@ -1,45 +1,45 @@
-import os
+# CAUTION: this script skips the 'new' folder for reasons so don't use that folder for anything
+# if you encounter any issues with the script please contact me @hozd in discord
 
-# set the root folder and the name of the ini file
-root_folder = os.path.dirname(os.path.abspath(__file__))
+import pathlib
+
+root_folder = pathlib.Path(__file__).resolve().parent
 ini_filename = "textures.ini"
 
+with open(root_folder / ini_filename, "w") as ini_file:
+    ini_file.write("# write your comments here\n\n[options]\n\nversion = 1\nhash = xxh64\nignoreAddress = True\nignoreMipmap = True\nreduceHash = True\n;video = True\n\n[games]\n\nUCES01421 = textures.ini\nUCUS98751 = textures.ini\nUCAS40318 = textures.ini\nNPJG00122 = textures.ini\n\n[hashes]\n\n; TEXTURES: Start\n\n")
+    
+    seen_paths = set()
+    seen_hashes = set()
+    undefined_paths = set()
 
-# loop through all subfolders in the root folder
-with open(os.path.join(root_folder, ini_filename), "w") as ini_file:
-    # add a start marker to the TEXTURES group, options, comments, games and allthat
-    ini_file.write(
-    "# https://github.com/Lin-zl522/Patapon-3-HD-Texture-Pack\n\n[options]\n\nversion = 1\nhash = xxh64\nignoreAddress = True\nignoreMipmap = True\nreduceHash = True\n;video = True\n\n[games]\nUCES01421 =textures.ini\nUCUS98751 = textures.ini\nUCAS40318 = textures.ini\nNPJG00122 = textures.ini\n\n[hashes]\n\n; TEXTURES: Start\n\n")
-    for folder, _, files in os.walk(root_folder):
-        for file in files:
-            # only include png files
-            if file.endswith(".png"):
-                # get the relative path to the png file
-                png_path = os.path.join(folder, file)
-                rel_path = os.path.relpath(png_path, root_folder).replace("\\", "/")
-                # get the texture hash from the png filename
-                texture_hash = file[:-4]
-                # check if the length is 26 (like [<hash>-1], it will delete the last two symbols)
-                if len(texture_hash) == 26:
-                    texture_hash = file[:-6]
-                    ini_file.write(f"#{texture_hash} = {rel_path}\n")
-                # otherwise check if the texture hash is valid and just add it
-                elif all(c in "0123456789ABCDEFabcdef" for c in texture_hash):
+    for png_path in root_folder.glob("**/*.png"):
+        rel_path = png_path.relative_to(root_folder).as_posix()
+        if "new" in rel_path.split('/'):
+            continue
+
+        texture_hash = png_path.stem[:24]
+        postfix = png_path.stem[24:]
+
+        if all(c.isdigit() or c.lower() in 'abcdef' for c in texture_hash) and len(texture_hash) == 24:
+            if postfix:
+                full_texture_hash = f"{texture_hash}{postfix}"
+                if full_texture_hash not in seen_hashes:
+                    ini_file.write(f"#{full_texture_hash} = {rel_path}\n")
+                    undefined_paths.add(rel_path)
+                    seen_hashes.add(full_texture_hash)
+            else:
+                if texture_hash not in seen_hashes:
                     ini_file.write(f"{texture_hash} = {rel_path}\n")
-    # add an end marker to the TEXTURES group
-    ini_file.write("\n; TEXTURES: End\n\n")
-    # add a start marker to the OTHER group
-    ini_file.write("; OTHER: Start\n\n")
-    for folder, _, files in os.walk(root_folder):
-        for file in files:
-            # only include files that are png and not already included in the TEXTURES group
-            if file.endswith(".png"):
-                png_path = os.path.join(folder, file)
-                rel_path = os.path.relpath(png_path, root_folder).replace("\\", "/")
-                texture_hash = file[:-4]
-                if not all(c in "0123456789ABCDEFabcdef" for c in texture_hash):
-                    # add the file to the OTHER group
-                    ini_file.write(f"#{rel_path}\n")
-    # add an end marker to the OTHER group and [hashranges]
-    ini_file.write("\n; OTHER: End\n\n[hashranges]")
+                    seen_hashes.add(texture_hash)
+            seen_paths.add(rel_path)
+    
+    ini_file.write("\n; TEXTURES: End\n")
 
+    if undefined_paths:
+        ini_file.write("\n; OTHER: Start\n\n")
+        for path in sorted(undefined_paths):
+            ini_file.write(f"#{path}\n")
+        ini_file.write("\n; OTHER: End\n")
+    
+    ini_file.write("\n[hashranges]")
