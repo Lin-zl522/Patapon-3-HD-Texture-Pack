2664d11749571ee743914dba53c196e321dbf03c
Hozzzd <92305508+Hozzzd@users.noreply.github.com>
2023-09-05 03:10:07 +0400

    totally should work now
    (i got no clue on what i'm doing)

--- textures.ini generator.py
+++ textures.ini generator.py
@@ -1,62 +1,68 @@
+# CAUTION: this script skips the 'new' folder for reasons so don't use that folder for anything
+# if you encounter any issues with the script please contact me @hozd in discord
+
 import pathlib
 
 root_folder = pathlib.Path(__file__).resolve().parent
 ini_filename = "textures.ini"
 
-def write_textures_section(ini_file, textures, header):
-    ini_file.write(f"\n; {header}: START\n")
-    for texture_hash, rel_path in sorted(textures.items(), key=lambda item: item[1]):
-        ini_file.write(f"{texture_hash} = {rel_path}\n")
-    ini_file.write(f"; {header}: END\n")
-
-def get_texture_hash(png_path):
-    texture_hash = png_path.stem[:24]
-    postfix = png_path.stem[24:]
-    if all(c.isdigit() or c.lower() in 'abcdef' for c in texture_hash) and len(texture_hash) == 24:
-        if postfix:
-            return f"{texture_hash}{postfix}"
-        else:
-            return texture_hash
-    return None
-
-def main():
-    custom_textures = {}
-    other_textures = {}
+with open(root_folder / ini_filename, "w") as ini_file:
+    ini_file.write("# write your comments here\n\n[options]\n\nversion = 1\nhash = xxh64\nignoreAddress = True\nignoreMipmap = True\nreduceHash = True\n;video = True\n\n[games]\n\nUCES01421 = textures.ini\nUCUS98751 = textures.ini\nUCAS40318 = textures.ini\nNPJG00122 = textures.ini\n\n[hashes]\n\n; TEXTURES: Start\n\n")
+    
     seen_paths = set()
+    seen_hashes = set()
+    undefined_paths = set()
 
+    # processing the custom textures first
     custom_textures_folder = root_folder / "_custom_textures"
-    for png_path in custom_textures_folder.glob("**/*.png"):
+    for png_path in custom_textures_folder.glob("*.png"):
         rel_path = png_path.relative_to(root_folder).as_posix()
-        texture_hash = get_texture_hash(png_path)
-        if texture_hash:
-            custom_textures[texture_hash] = rel_path
+        texture_hash = png_path.stem[:24]
+        postfix = png_path.stem[24:]
 
+        if all(c.isdigit() or c.lower() in 'abcdef' for c in texture_hash) and len(texture_hash) == 24:
+            if postfix:
+                full_texture_hash = f"{texture_hash}{postfix}"
+                if full_texture_hash not in seen_hashes:
+                    ini_file.write(f"#{full_texture_hash} = {rel_path}\n")
+                    undefined_paths.add(rel_path)
+                    seen_hashes.add(full_texture_hash)
+            else:
+                if texture_hash not in seen_hashes:
+                    ini_file.write(f"{texture_hash} = {rel_path}\n")
+                    seen_hashes.add(texture_hash)
+            seen_paths.add(rel_path)
+            
+    # then everything else (if you want to make tihs script more optimized, be my guest)
     for png_path in root_folder.glob("**/*.png"):
         rel_path = png_path.relative_to(root_folder).as_posix()
         if "new" in rel_path.split('/'):
             continue
+        if custom_textures_folder in png_path.parents:
+            continue
 
-        texture_hash = get_texture_hash(png_path)
-        if texture_hash and rel_path not in seen_paths:
-            if texture_hash in custom_textures:
-                continue
-            other_textures[texture_hash] = rel_path
+        texture_hash = png_path.stem[:24]
+        postfix = png_path.stem[24:]
+
+        if all(c.isdigit() or c.lower() in 'abcdef' for c in texture_hash) and len(texture_hash) == 24:
+            if postfix:
+                full_texture_hash = f"{texture_hash}{postfix}"
+                if full_texture_hash not in seen_hashes:
+                    ini_file.write(f"#{full_texture_hash} = {rel_path}\n")
+                    undefined_paths.add(rel_path)
+                    seen_hashes.add(full_texture_hash)
+            else:
+                if texture_hash not in seen_hashes:
+                    ini_file.write(f"{texture_hash} = {rel_path}\n")
+                    seen_hashes.add(texture_hash)
             seen_paths.add(rel_path)
+    
+    ini_file.write("\n; TEXTURES: End\n")
 
-    with open(root_folder / ini_filename, "w") as ini_file:
-        ini_file.write("# write your comments here\n\n[options]\n\nversion = 1\nhash = xxh64\nignoreAddress = True\nignoreMipmap = True\nreduceHash = True\n;video = True\n\n[games]\n\nUCES01421 = textures.ini\nUCUS98751 = textures.ini\nUCAS40318 = textures.ini\nNPJG00122 = textures.ini\n\n[hashes]\n\n")
-
-        write_textures_section(ini_file, other_textures, "TEXTURES")
-        
-        write_textures_section(ini_file, custom_textures, "CUSTOM TEXTURES")
-
-        if other_textures:
-            ini_file.write("\n; OTHER: Start\n\n")
-            for rel_path in sorted(seen_paths - set(other_textures.values())):
-                ini_file.write(f"#{rel_path}\n")
-            ini_file.write("\n; OTHER: End\n")
-
-        ini_file.write("\n[hashranges]")
-
-if __name__ == "__main__":
-    main()
+    if undefined_paths:
+        ini_file.write("\n; OTHER: Start\n\n")
+        for path in sorted(undefined_paths):
+            ini_file.write(f"#{path}\n")
+        ini_file.write("\n; OTHER: End\n")
+    
+    ini_file.write("\n[hashranges]")
